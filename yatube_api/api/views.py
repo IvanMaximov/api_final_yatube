7f5ddc76d4075011edc8c50215101dc6ac22e6aa
Иван Максимов <ivanmaksimov02@mail.ru>
2022-02-19 13:02:09 +0300

    api

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from posts.models import Group, Post, User, Follow
 from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 
+from posts.models import Group, Post
 from api.permissions import OwnerOrReadOnly, ReadOnly
 from api.serializers import (CommentSerializer, FollowSerializer,
                              GroupSerializer, PostSerializer)
